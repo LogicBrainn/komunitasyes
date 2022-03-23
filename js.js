1aecbe401c482e95cba6af48c24dcb1471028394
logicbrainn <logicbrain.exe@gmail.com>
2022-03-24 01:39:04 +0800

    padding top contaner

--- js.js
+++ js.js
@@ -5,14 +5,14 @@ function klik(){
     for(i =1 ; i<=5;i++){
         if(but=="navbar-toggler"){
         con= document.getElementById("container"+i)
-        con.style.paddingTop= "51vh";
+        con.style.paddingTop= "60vh";
         if(i==5){
             but="navbar-toggler 2"
         }
         }
         else if(but=="navbar-toggler 2"){
         con= document.getElementById("container"+i);
-        con.style.paddingTop= "15vh";
+        con.style.paddingTop= "25vh";
          if (i==5){
              but="navbar-toggler"
          }
