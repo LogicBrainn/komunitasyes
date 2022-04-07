de57f91b415ace1211df1867892ca5798e8c5a08
logicbrainn <logicbrain.exe@gmail.com>
2022-04-08 02:43:32 +0800

    bagian Webinar

--- js.js
+++ js.js
@@ -1,24 +1,3 @@
-var but = document.getElementById("but").classList;
-var con;
-function klik(){
-
-    for(i =1 ; i<=5;i++){
-        if(but=="navbar-toggler"){
-        con= document.getElementById("container"+i)
-        con.style.paddingTop= "60vh";
-        if(i==5){
-            but="navbar-toggler 2"
-        }
-        }
-        else if(but=="navbar-toggler 2"){
-        con= document.getElementById("container"+i);
-        con.style.paddingTop= "25vh";
-         if (i==5){
-             but="navbar-toggler"
-         }
-        }
-    }
-}
 
 
 
