07b64173f00cf6ebed59ad2ad4fdeb2909b86bec
logicbrainn <logicbrain.exe@gmail.com>
2022-04-10 02:01:05 +0800

    animasi arah kebawah

--- js.js
+++ js.js
@@ -1,3 +1,19 @@
+var down = document.getElementById("down");
+
+setInterval(() =>{
+    if(down.style.marginTop=="0vh"){
+        down.style.marginTop="6vh";
+    }
+
+     else if(down.style.marginTop=="6vh"){
+        down.style.marginTop="0vh";
+    }
+    
+
+},1000);
+
+    
+
 
 
 
